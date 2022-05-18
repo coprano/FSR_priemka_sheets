61fc141855a2dfd119643fd5296f6303065dba06
coprano <73130317+coprano@users.noreply.github.com>
2022-05-18 17:48:52 +0300

    added a way to find a necessary sheet

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from svodka import *
 from grant_access import *
 import settings
 
-svodka(settings.datafilepath)
-spreadsheetId = "1BqGdb75jl33yFq1E75tnJ36d0IsXhSyMw9ZtcUUixhU"
-email = "fsr.bot.priemka@gmail.com"
-grant_access(spreadsheetId,email)
+#на какой лист писать?
+listname = "Лист1"
+
+svodka(settings.datafilepath,settings.SHEET_ID,listname)
+
+# email = "fsr.bot.priemka@gmail.com"
+# grant_access(spreadsheetId,email)
--- svodka.py
+++ svodka.py
@@ -11,8 +11,7 @@ import httplib2
 from googleapiclient import discovery
 from oauth2client.service_account import ServiceAccountCredentials	
 
-print('Мы будем использовать лист с Id = ', sheetId)
-def svodka(file_path):
+def svodka(file_path,spreadsheetId,listname):
     """
     Создает новую гугл таблицу и выгружает в неё файл
     Выгружает полный csv файл в НОВУЮ гугл таблицу
@@ -31,19 +30,16 @@ def svodka(file_path):
                     line_count += 1
                 else:
                     line_count += 1
-                print(f'Processed {line_count} lines.')
-            logging.info(f'Processed {line_count} lines.')
-        logging.info(f"SVODKA: processed {file_path}")
-        spreadsheet = service.spreadsheets().create(body = {
-            'properties': {'title': 'Первый тестовый документ', 'locale': 'ru_RU'},
-            'sheets': [{'properties': {'sheetType': 'GRID',
-                                       'sheetId': 0,
-                                       'title': 'Лист номер один',
-                                       'gridProperties': {'rowCount': 100, 'columnCount': 15}}}]
-        }).execute()
-        spreadsheetId = spreadsheet['spreadsheetId'] # сохраняем идентификатор файла
-        print('https://docs.google.com/spreadsheets/d/' + spreadsheetId)
-        return spreadsheetId
+            logging.info(f'SVODKA: processed {line_count} lines.')
+        logging.info(f"SVODKA: finished processing {file_path}")
+        # Получаем список листов, их Id и название
+        spreadsheet = service.spreadsheets().get(spreadsheetId = spreadsheetId).execute()
+        sheetList = spreadsheet.get('sheets')
+        # for sheet in sheetList:
+        #     print(sheet['properties']['sheetId'], sheet['properties']['title'])
+        for sheet in sheetList:
+           if sheet['properties']['title'] == listname:
+               print(sheet['properties']['sheetId'], sheet['properties']['title'])
     else:
         logging.exception(f"SVODKA: couldn't open file:{file_path}")
         return -1
